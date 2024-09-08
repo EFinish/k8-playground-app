e2a4b74d9800b2c6b50dac22a27099d8b7c78182
EFinish <EFinish@gmail.com>
2024-09-08 16:15:58 +0200

    super basic go service

---
+++ counter-server/go.mod
@@ -0,0 +1,3 @@
+module counter-server
+
+go 1.23.1
---
+++ counter-server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}

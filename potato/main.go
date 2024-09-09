9fccc008b7098443c72a28f31e43141db4a754f2
EFinish <EFinish@gmail.com>
2024-09-09 22:07:14 +0200

    deployment file

--- potato/main.go
+++ potato/main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
+	// http.Handle()
+
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)

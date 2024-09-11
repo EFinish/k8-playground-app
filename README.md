f0f36569ce91216f4edc75fbee77e7da187bcc4e
EFinish <EFinish@gmail.com>
2024-09-11 19:12:06 +0200

    make dockerfile service html, new manifest files in manifests folder, instructions for how to run locally via minikube

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # k8-playground-app
+
+## steps to run locally
+1. install minikube and kubectl locally
+2. `minikube start`
+3. `cd manifests`
+4. `kubectl apply -f potato-deployment.yml`
+5. `kubectl apply -f potato-load-balancer.yml`
+6. `minikube service potatoweb-load-balancer --url` and open the url given by the command
--- potato/main.go
+++ potato/main.go
@@ -5,10 +5,19 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
 
-	// http.Handle()
+func servePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func main() {
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(servePage)))
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
